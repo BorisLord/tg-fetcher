b711a539abde733f45403023c81006f61f5e0a4a
bopad <borisdelord@proton.me>
2025-03-03 23:55:36 +0100

    add bitget and update cron job schedule

--- src/config/defined_exchanges.ts
+++ src/config/defined_exchanges.ts
@@ -1,1 +1,1 @@
-export const definedExchanges = ["phemex", "coinbase"];
+export const definedExchanges = ["phemex", "coinbase", "bitget"];
--- src/jobs/cron_jobs.ts
+++ src/jobs/cron_jobs.ts
@@ -1,5 +1,6 @@
 import { forcedLoadMarkets } from "../services/exchanges_service.ts";
 
-// Deno.cron("Cron loadMarkets forced", "* * * * *", () => {
-//   forcedLoadMarkets();
-// });
+Deno.cron("Cron loadMarkets forced", "0 * * * *", () => {
+  console.log("cron start");
+  forcedLoadMarkets();
+});
