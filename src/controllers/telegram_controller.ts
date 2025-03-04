6563d2513a6c8352301592d7f6c590fe8b2f6c52
bopad <borisdelord@proton.me>
2025-03-04 01:58:09 +0100

    store value into kv ddb

--- src/controllers/telegram_controller.ts
+++ src/controllers/telegram_controller.ts
@@ -2,6 +2,8 @@ import client from "../config/init_telegram.ts";
 import "@std/dotenv/load";
 import { messageSchema } from "../validations/validation_Uly.ts";
 import { db } from "../config/init_dbs.ts";
+import { format } from "jsr:@std/datetime/format";
+import type { availability } from "../types/index.ts";
 
 const chatId = parseInt(Deno.env.get("CHAT_ID")!);
 
@@ -12,18 +14,39 @@ export const listenMessage = () => {
       if (message.chat.id === chatId) {
         try {
           // console.log("pre validate", message);
-          const validateMsg = messageSchema.parseAsync(message);
-          console.log("post validate", await validateMsg);
-          // await db.set(
-          //   ["validateMsg"],
-          //   validateMsg,
-          // );
+          const validateMsg = await messageSchema.parseAsync(message);
+          console.log("post validate", validateMsg);
+
+          const currentDate = format(new Date(), "yyyy-MM-dd"); // Format YYYY-MM-DD
+
+          for (const order of validateMsg.text) {
+            if (order.orderType !== null) {
+              const ordersKey = [
+                validateMsg.chat.id,
+                "orders",
+                order.orderType,
+                currentDate,
+              ];
+
+              // Récupérer les ordres existants pour ce type et cette date
+              const existingOrders = await db.get<availability[]>(ordersKey) ||
+                [];
+
+              // Mettre à jour les ordres
+              const updatedOrders = existingOrders?.value
+                ? [...existingOrders.value, order]
+                : [order];
+
+              // Stocker les ordres mis à jour
+              await db.set(ordersKey, updatedOrders);
+            }
+          }
         } catch (err) {
           console.log("error:", err);
         }
 
-        // const v = await db.get(["validateMsg"]);
-        // console.log("vvvvvvvvvvvv", v);
+        const v = await db.get([chatId, "orders"]);
+        console.log("vvvvvvvvvvvv", v);
 
         // Deno.writeTextFile(
         //   "./app/channel_messages.json",
